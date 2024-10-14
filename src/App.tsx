2712a9070dc0e716abaf1da407d4fa1a1bef515f
ian <ian@soulware.kr>
2024-10-14 21:05:09 +0900

    feat: 기본적인 기능 추가
    이미지 목록, 이미지 크게 보기, 선택 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,122 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {
   Dimensions,
   FlatList,
   Image,
   StyleSheet,
+  Text,
   TouchableOpacity,
   View,
 } from 'react-native';
 
-const images = [
-  {id: '1', uri: 'https://via.placeholder.com/600x400?text=Image+1'},
-  {id: '2', uri: 'https://via.placeholder.com/600x400?text=Image+2'},
-  {id: '3', uri: 'https://via.placeholder.com/600x400?text=Image+3'},
-  {id: '4', uri: 'https://via.placeholder.com/600x400?text=Image+4'},
-  {id: '5', uri: 'https://via.placeholder.com/600x400?text=Image+5'},
-];
-const App = () => {
-  const [selectedImage, setSelectedImage] = useState(images[0].uri);
+const API_URL = 'https://api.pexels.com/v1/search?query=nature';
 
-  const renderImage = ({item}: {item: {id: string; uri: string}}) => (
-    <TouchableOpacity onPress={() => setSelectedImage(item.uri)}>
-      <Image source={{uri: item.uri}} style={styles.thumbnail} />
-    </TouchableOpacity>
-  );
+const fetchImagesFromPexels = async () => {
+  const response = await fetch(API_URL, {
+    headers: {
+      Authorization: 'knUU0k6XlgRkwPSBxI5JMhik215sS6aamXkHmTQARu2yXWzQMAi182GN',
+    },
+  });
+
+  const data = await response.json();
+  return data.photos;
+};
+
+const {width, height} = Dimensions.get('screen');
+
+const App = () => {
+  const [images, setImages] = useState(null);
+  const topRef = useRef();
+  const thumbRef = useRef();
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const IMAGE_SIZE = 80;
+  const SPACING = 12;
+
+  const scrollActiveIndex = index => {
+    setActiveIndex(index);
+    topRef?.current?.scrollToOffset({
+      offset: index * width,
+      animated: true,
+    });
+
+    if (index * (80 + 12) - 80 / 2 > width / 2) {
+      thumbRef?.current?.scrollToOffset({
+        offset: index * (IMAGE_SIZE + SPACING) - width / 2 + IMAGE_SIZE / 2,
+        animated: true,
+      });
+    } else {
+        thumbRef?.current?.scrollToOffset({
+            offset: 0,
+            animated: true,
+          });
+    }
+  };
+  useEffect(() => {
+    const fetchImages = async () => {
+      const fetchedImages = await fetchImagesFromPexels();
+      setImages(fetchedImages);
+      console.log('images', fetchedImages);
+    };
+
+    fetchImages();
+  }, []);
+
+  if (!images) {
+    return <Text>Loading...</Text>;
+  }
 
   return (
-    <View style={styles.container}>
-      {/* <View style={styles.fullImageContainer}>
-        <Image source={{uri: selectedImage}} style={styles.fullImage} />
-      </View> */}
-      {/* 작은 사진 슬라이더 */}
-      <View style={{height: 120}}>
-        <FlatList
-          data={images}
-          renderItem={renderImage}
-          keyExtractor={item => item.id}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          style={styles.thumbnailContainer}
-        />
-      </View>
+    <View style={{flex: 1, backgroundColor: '#000'}}>
+      <FlatList
+        ref={topRef}
+        data={images}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={ev => {
+          scrollActiveIndex(Math.floor(ev.nativeEvent.contentOffset.x / width));
+        }}
+        renderItem={({item}) => {
+          return (
+            <View style={{width, height}}>
+              <Image
+                source={{uri: item.src.portrait}}
+                style={[StyleSheet.absoluteFillObject]}
+              />
+            </View>
+          );
+        }}
+      />
+      <FlatList
+        ref={thumbRef}
+        data={images}
+        keyExtractor={item => item.id.toString()}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        style={{position: 'absolute', bottom: 80}}
+        contentContainerStyle={{paddingHorizontal: 10}}
+        renderItem={({item, index}) => {
+          return (
+            <TouchableOpacity onPress={() => scrollActiveIndex(index)}>
+              <Image
+                source={{uri: item.src.portrait}}
+                style={{
+                  width: IMAGE_SIZE,
+                  height: IMAGE_SIZE,
+                  borderRadius: SPACING,
+                  marginRight: 10,
+                  borderWidth: 2,
+                  borderColor: activeIndex === index ? '#fff' : '#000',
+                }}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  fullImageContainer: {
-    marginTop: 10,
-    marginLeft: 100,
-    marginRight: 100,
-  },
-  fullImage: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height * 0.5,
-    resizeMode: 'cover',
-  },
-  thumbnailContainer: {
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  thumbnail: {
-    width: 100,
-    height: 100,
-    marginHorizontal: 5,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-});
 
 export default App;
